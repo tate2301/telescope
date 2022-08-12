a1db5367be91b45c5067e25b73ba010baecd1343
Kamfeskaya <tatendachris@gmail.com>
2022-08-12 05:03:57 +0200

    feat: add watermark

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
 import { AppProps /* , AppContext */ } from 'next/app';
 
 import 'styles/globals.css';
@@ -5,6 +6,16 @@ import 'styles/globals.css';
 function App({ Component, pageProps }: AppProps) {
   return (
     <div className="w-screen min-h-screen bg-white dark:text-gray-50 dark:bg-slate-900">
+      <div className="fixed right-0 hidden p-3 lg:flex bottom-24">
+        <a
+          href="https://kamfeskaya.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="px-3 py-2 text-sm font-medium bg-gray-100 border-2 rounded-lg dark:text-gray-50 text-slate-800 dark:border-gray-700 dark:bg-slate-800"
+        >
+          Kamfeskaya Ecosystem
+        </a>
+      </div>
       <Component {...pageProps} />
     </div>
   );
