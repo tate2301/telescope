699ab29debf6652cd17fa885e8324c0d9cffeb54
Kamfeskaya <tatendachris@gmail.com>
2023-02-18 14:21:44 +0200

    feat: add url shortener

---
+++ components/url-shortener/ShortURLCard.tsx
@@ -0,0 +1,24 @@
+interface ShortURLCardProps {
+  title: string;
+  url: string;
+  short_url: string;
+}
+
+export default function ShortURLCard({
+  title,
+  url,
+  short_url,
+}: ShortURLCardProps) {
+  return (
+    <div className="flex flex-col gap-4 p-4 border rounded-lg shadow border-zinc-50 bg-zinc-100">
+      <div className="w-12 bg-gray-200 rounded-lg aspect-square" />
+      <div className="flex flex-col gap-1">
+        <p className="text-lg sf">{title}</p>
+        <div>
+          <p>{url}</p>
+          <p>{short_url}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/preview/shortener.tsx
+++ pages/preview/shortener.tsx
@@ -1,6 +1,7 @@
 import Footer from 'components/footer';
 import { PublicNavbar } from 'components/navbar';
 import SEO from 'components/seo';
+import ShortURLCard from 'components/url-shortener/ShortURLCard';
 import GradientPageBackground from 'components/utils/GradientPageBackground';
 
 export default function Shortener() {
@@ -11,7 +12,41 @@ export default function Shortener() {
 
       <div className="container relative flex flex-col items-center justify-center max-w-3xl min-h-screen px-2 py-0 mx-auto subpixel-antialiased font-normal">
         <SEO pageTitle={'URL Shortener'} />
-        <main className="flex flex-col flex-1 w-full px-0 pt-8 pb-20 overflow-x-hidden"></main>
+        <main className="flex flex-col flex-1 w-full gap-16 px-4 pt-16 overflow-x-hidden">
+          <div className="flex flex-col gap-2">
+            <h1 className="text-3xl">URL Plastic Surgery</h1>
+            <p>
+              This tool allows you to create beautiful AKA short urls. New URLs
+              will be stored for 72 hours and then automatically deleted. You
+              can sign in if you want to &qout;permanently&qout; store your
+              URLs.
+            </p>
+          </div>
+          <div className="flex gap-2">
+            <input
+              className="input input-filled"
+              placeholder="Enter your url"
+              name="url"
+              required
+            />
+            <button className="btn btn-action">Kaboom</button>
+          </div>
+          <div className="flex flex-col gap-8 ">
+            <h2 className="text-xl">Recent patients(URLs)</h2>
+            <ShortURLCard
+              title={'Stripe - Online Payments Platform'}
+              short_url={'https://zicheni.io/stripe/8s9bn23'}
+              url={
+                'https://stripe.com/sessions/2021/09/20/online-payments-stuff'
+              }
+            />
+            <ShortURLCard
+              title={'Tatenda Chinyamakobvu - Product designer & Developer'}
+              short_url={'https://zicheni.io/kamfes/2yh9d28nf829'}
+              url={'https://kamfeskaya.com/projects/paynow-react'}
+            />
+          </div>
+        </main>
       </div>
       <Footer />
     </>
